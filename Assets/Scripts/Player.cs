2796500a1f1e299a2b8ce7087b39d680c0cb528e
Spencer <sbarton9258@gmail.com>
2019-04-18 11:22:33 -0700

    Muneh

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
     public GameObject dice1, dice2;
 
     public GameObject Button;
+
     // Start is called before the first frame update
     public GameObject[] waypointArray;
     private Waypoint currentWaypoint;
@@ -19,94 +20,98 @@ public class Player : MonoBehaviour
     public int stopRolling = 1;
     public int wallet = 1500;
     public String ownership;
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
     // placeholders / future //
 
     public String tileType;
     public int rentvalue;
 
-    void actions(){
+    void actions()
+    {
     }
 
-     void ownerCheck()
+    void ownerCheck()
     {
     }
 
 
 
-     void CCorChance()
+    void CCorChance()
     {
     }
 
     // end of placeholdes // 
 
-     void payrent()
+    void payrent()
     {
         wallet = wallet - rentvalue;
-        
+
     }
-    
+
     void Start()
     {
-        
-        transform.position = new Vector3(waypointArray[waypointIndex].transform.position.x, waypointArray[waypointIndex].transform.position.y, 0);
-        
+
+        transform.position = new Vector3(waypointArray[waypointIndex].transform.position.x,
+            waypointArray[waypointIndex].transform.position.y, 0);
+
         ownerCheck(); // placeholder//
 
-        if (ownership == "available"){
-            
-        }
-        if (ownership == "yours"){
-            actions(); // placeholder//
-            }
 
-        if (ownership == "theirs"){
-            payrent(); // placehholder//
-            actions();
-        }
 
-        if (ownership == "unownable")
+
+
+        // Update is called once per frame
+        public void followWaypoints()
         {
-            if (tileType == "CC" & tileType == "Chance")  {
-                CCorChance();
-            }
-            actions();
-        }
-    }
-    
-    
-    
-    // Update is called once per frame
-    public void followWaypoints()
-    {
-        if (stopRolling == 1)
-        {
-            for (int i = 0; i < dice2.GetComponent<Dice>().value + dice1.GetComponent<Dice>().value; i++)
+            if (stopRolling == 1)
             {
-                transform.position = new Vector3(waypointArray[waypointIndex + 1].transform.position.x,
-                    waypointArray[waypointIndex + 1].transform.position.y, 0);
-                waypointIndex++;
+                for (int i = 0; i < dice2.GetComponent<Dice>().value + dice1.GetComponent<Dice>().value; i++)
+                {
+                    transform.position = new Vector3(waypointArray[waypointIndex + 1].transform.position.x,
+                        waypointArray[waypointIndex + 1].transform.position.y, 0);
+                    waypointIndex++;
+                }
+
+                stopRolling = 2;
             }
-
-            stopRolling = 2;
+            else
+            {
+                stopRolling = 1;
+            }
         }
-        else
+
+        void Update()
         {
-            stopRolling = 1;
+
         }
-    }
-    void Update()
-    {
-        
-    }
 
-    
-    
-    
 
+        /*  
+          if (ownership == "available"){
+                  
+          }
+          if (ownership == "yours"){
+              actions(); // placeholder//
+          }
+      
+          if (ownership == "theirs"){
+              payrent(); // placehholder//
+              actions();
+          }
+      
+          if (ownership == "unownable")
+          {
+              if (tileType == "CC" & tileType == "Chance")  {
+                  CCorChance();
+              }
+              actions();
+          }
+          }
+          */
+    }
 }
