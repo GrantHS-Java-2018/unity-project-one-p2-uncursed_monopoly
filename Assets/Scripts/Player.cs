ac21b9b11737e21d3d57006f5d1604044e68075d
Spencer <sbarton9258@gmail.com>
2019-04-18 11:46:01 -0700

    Muneh

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
-// using NUnit.Framework.Internal;
-using TMPro.EditorUtilities;
+//using NUnit.Framework.Internal;
+// using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -16,8 +16,7 @@ public class Player : MonoBehaviour
     public GameObject[] waypointArray;
     private Waypoint currentWaypoint;
     private int currentLocation = 0;
-    private int waypointIndex = 0;
-<<<<<<< HEAD
+    private int waypointIndex;
     public int stopRolling = 1;
     public int wallet = 1500;
     public String ownership;
@@ -33,12 +32,6 @@ public class Player : MonoBehaviour
     public int rentvalue;
 
     void actions()
-=======
-    public int stopRolling = 1; 
-    int doublesCounter = 0;
-    public GameObject jailWaypoint;
-    void Start()
->>>>>>> master
     {
     }
 
@@ -66,21 +59,17 @@ public class Player : MonoBehaviour
         transform.position = new Vector3(waypointArray[waypointIndex].transform.position.x,
             waypointArray[waypointIndex].transform.position.y, 0);
 
-        ownerCheck(); // placeholder//
+    }
+    //ownerCheck(); // placeholder//
 
 
 
 
 
         // Update is called once per frame
-        public void followWaypoints()
+         public void followWaypoints()
         {
-<<<<<<< HEAD
             if (stopRolling == 1)
-=======
-            Doubles();
-            for (int i = 0; i < dice2.GetComponent<Dice>().value + dice1.GetComponent<Dice>().value; i++)
->>>>>>> master
             {
                 for (int i = 0; i < dice2.GetComponent<Dice>().value + dice1.GetComponent<Dice>().value; i++)
                 {
@@ -102,7 +91,6 @@ public class Player : MonoBehaviour
 
         }
 
-<<<<<<< HEAD
 
         /*  
           if (ownership == "available"){
@@ -126,51 +114,6 @@ public class Player : MonoBehaviour
           }
           }
           */
-=======
-       
-         void Doubles()
-         {
-             Boolean doubles;
-             if (dice2.GetComponent<Dice>().value == dice1.GetComponent<Dice>().value)
-             {
-                 doubles = true;
-                 if (doublesCounter < 3)
-                         {
-                             doublesCounter++;
-                             if (doublesCounter == 3)
-                             {
-                                 transform.position = new Vector3(jailWaypoint.transform.position.x,
-                                     waypointArray[waypointIndex + 1].transform.position.y, 0);
-                                 Button.SetActive(false); 
-                             }
-                             else
-                             {
-                                 //GetComponent<Renderer>().enabled = true;
-                                 Button.SetActive(true);
-                             }
-                         }
-                         else
-                         {
-                             //GetComponent<Renderer>().enabled = false;
-                             Button.SetActive(false); 
-                             doublesCounter = 0;
-                         }
-             }
-             else
-             {
-                 doubles = false;
-                 Button.SetActive(false); 
-             }
-             
         
-        }
-
-        
-    }
-
-    void Update()
-    {
-        
->>>>>>> master
-    }
 }
+
