21d53494bf9940f08bcd1138849543f3c37138b6
Kirk <kjid.dibert@gmail.com>
2019-04-26 10:14:28 -0700

    Started property class

---
+++ Assets/Scripts/Properties.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Properties : MonoBehaviour
+{
+    [SerializeField]private Sprite[] properyCards;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+}
