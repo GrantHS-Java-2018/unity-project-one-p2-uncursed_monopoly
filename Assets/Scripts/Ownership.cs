3e993b59e5cdbf4e69e0964f468fa1a55e269c7e
Kirk <kjid.dibert@gmail.com>
2019-05-29 10:35:04 -0700

    Rolling

--- Assets/Scripts/Ownership.cs
+++ Assets/Scripts/Ownership.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class Ownership : MonoBehaviour
 {
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Player3;
+    public GameObject dice1, dice2;
+
+    public int[] playerOwnership;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CheckOwnership(int player)
+    {
+       if (dice1.GetComponent<Dice>().playerList[player-1])
     }
 
 }
